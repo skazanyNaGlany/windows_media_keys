8c7134fc14f59cef99100af8f0718508eb56a8e3
unknown <screamingbox@gmail.com>
2023-08-13 17:46:08 +0200

    Added support for suspend, restart and poweroff keys

--- media_keys_emulator.go
+++ media_keys_emulator.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os/exec"
 	"time"
 
 	"golang.org/x/sys/windows"
@@ -11,6 +12,9 @@ const VK_DOWN = 0x28
 const VK_UP = 0x26
 const VK_RIGHT = 0x27
 const VK_LEFT = 0x25
+const VK_F4 = 0x73
+const VK_F7 = 0x76
+const VK_F8 = 0x77
 const VK_F11 = 0x7A
 const VK_F12 = 0x7B
 const VK_VOLUME_DOWN = 0xAE
@@ -19,11 +23,24 @@ const VK_MEDIA_NEXT_TRACK = 0xB0
 const VK_MEDIA_PREV_TRACK = 0xB1
 const VK_VOLUME_MUTE = 0xAD
 const VK_MEDIA_PLAY_PAUSE = 0xB3
+const VK_SLEEP = 0x5F
+const VK_OEM_RESET = 0xE9
+
+type OnActionHook func() bool
 
 type MediaKeysEmulator struct {
 	modUser32            *windows.LazyDLL
 	procGetAsyncKeyState *windows.LazyProc
 	procKeybdEvent       *windows.LazyProc
+	volumeDownHook       OnActionHook
+	volumeUpHook         OnActionHook
+	mediaNextHook        OnActionHook
+	mediaPrevHook        OnActionHook
+	sleepHook            OnActionHook
+	restartHook          OnActionHook
+	shutdownHook         OnActionHook
+	muteHook             OnActionHook
+	playHook             OnActionHook
 }
 
 func (mke *MediaKeysEmulator) Init() {
@@ -45,11 +62,18 @@ func (mke *MediaKeysEmulator) EmulateInLoop() {
 		upPressed := mke.CallGetAsyncKeyState(VK_UP)
 		rightPressed := mke.CallGetAsyncKeyState(VK_RIGHT)
 		leftPressed := mke.CallGetAsyncKeyState(VK_LEFT)
+		f4Pressed := mke.CallGetAsyncKeyState(VK_F4)
+		f7Pressed := mke.CallGetAsyncKeyState(VK_F7)
+		f8Pressed := mke.CallGetAsyncKeyState(VK_F8)
 		f11Pressed := mke.CallGetAsyncKeyState(VK_F11)
 		f12Pressed := mke.CallGetAsyncKeyState(VK_F12)
 
 		if downPressed != 0 {
 			// volume down
+			if mke.volumeDownHook != nil && !mke.volumeDownHook() {
+				continue
+			}
+
 			mke.procKeybdEvent.Call(
 				uintptr(VK_VOLUME_DOWN),
 				uintptr(0),
@@ -57,6 +81,10 @@ func (mke *MediaKeysEmulator) EmulateInLoop() {
 				uintptr(0))
 		} else if upPressed != 0 {
 			// volume up
+			if mke.volumeUpHook != nil && !mke.volumeUpHook() {
+				continue
+			}
+
 			mke.procKeybdEvent.Call(
 				uintptr(VK_VOLUME_UP),
 				uintptr(0),
@@ -64,6 +92,10 @@ func (mke *MediaKeysEmulator) EmulateInLoop() {
 				uintptr(0))
 		} else if rightPressed != 0 {
 			// media next
+			if mke.mediaNextHook != nil && !mke.mediaNextHook() {
+				continue
+			}
+
 			mke.procKeybdEvent.Call(
 				uintptr(VK_MEDIA_NEXT_TRACK),
 				uintptr(0),
@@ -71,13 +103,42 @@ func (mke *MediaKeysEmulator) EmulateInLoop() {
 				uintptr(0))
 		} else if leftPressed != 0 {
 			// media prev
+			if mke.mediaPrevHook != nil && !mke.mediaPrevHook() {
+				continue
+			}
+
 			mke.procKeybdEvent.Call(
 				uintptr(VK_MEDIA_PREV_TRACK),
 				uintptr(0),
 				uintptr(0),
 				uintptr(0))
+		} else if f4Pressed != 0 {
+			// sleep
+			if mke.sleepHook != nil && !mke.sleepHook() {
+				continue
+			}
+
+			exec.Command("rundll32.exe", "powrprof.dll,SetSuspendState", "0,1,0").Start()
+		} else if f7Pressed != 0 {
+			// restart
+			if mke.restartHook != nil && !mke.restartHook() {
+				continue
+			}
+
+			exec.Command("shutdown", "/r", "/t", "0").Start()
+		} else if f8Pressed != 0 {
+			// shutdown
+			if mke.shutdownHook != nil && !mke.shutdownHook() {
+				continue
+			}
+
+			exec.Command("shutdown", "/s", "/t", "0").Start()
 		} else if f11Pressed != 0 {
 			// mute/unmute
+			if mke.muteHook != nil && !mke.muteHook() {
+				continue
+			}
+
 			mke.procKeybdEvent.Call(
 				uintptr(VK_VOLUME_MUTE),
 				uintptr(0),
@@ -85,6 +146,10 @@ func (mke *MediaKeysEmulator) EmulateInLoop() {
 				uintptr(0))
 		} else if f12Pressed != 0 {
 			// play/pause
+			if mke.playHook != nil && !mke.playHook() {
+				continue
+			}
+
 			mke.procKeybdEvent.Call(
 				uintptr(VK_MEDIA_PLAY_PAUSE),
 				uintptr(0),
